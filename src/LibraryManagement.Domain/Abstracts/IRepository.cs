3e47627d61356d87d15118e64e4259ed5f203934
mfaour <mohammad.alfaour@optimumpartners.co>
2025-07-06 07:13:42 +0300

    Add `BaseRepository` and `IRepository<TEntity>` abstarct class and interface.

---
+++ src/LibraryManagement.Domain/Abstracts/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryManagement.Domain.Abstracts;
+
+public interface IRepository<TEntity> where TEntity : class
+{
+    Task<List<TEntity>> GetListAsync();
+    Task<TEntity?> GetAsync(Guid id);
+    Task<TEntity> AddAsync(TEntity entity);
+    Task<TEntity> UpdateAsync(TEntity entity);
+    Task DeleteAsync(TEntity entity);
+}
---
+++ src/LibraryManagement.Infrastructure/Data/BaseRepository.cs
@@ -0,0 +1,36 @@
+﻿using LibraryManagement.Domain.Abstracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagement.Infrastructure.Data;
+
+internal abstract class BaseRepository<TEntity>(LibraryManagementDbContext dbContext) : IRepository<TEntity> where TEntity : class
+{
+    protected DbSet<TEntity> DbSet { get; } = dbContext.Set<TEntity>();
+    protected Task<int> SaveChangesAsync() => dbContext.SaveChangesAsync();
+
+    public virtual async Task<List<TEntity>> GetListAsync() => await DbSet.ToListAsync();
+
+    public virtual async Task<TEntity?> GetAsync(Guid id) => await DbSet.FindAsync(id);
+
+    public virtual async Task<TEntity> AddAsync(TEntity entity)
+    {
+        await DbSet.AddAsync(entity);
+        await SaveChangesAsync();
+
+        return entity;
+    }
+
+    public virtual async Task<TEntity> UpdateAsync(TEntity entity)
+    {
+        DbSet.Update(entity);
+        await SaveChangesAsync();
+
+        return entity;
+    }
+
+    public virtual async Task DeleteAsync(TEntity entity)
+    {
+        DbSet.Remove(entity);
+        await SaveChangesAsync();
+    }
+}
